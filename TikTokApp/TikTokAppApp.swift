155aeb1b12a1d760ebe42fdff705a70ec4d8772c
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-10-22 14:38:37 +0500

    Initial Commit

---
+++ TikTokApp/TikTokAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokAppApp.swift
+//  TikTokApp
+//
+//  Created by Muhammad Khan on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct TikTokAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
